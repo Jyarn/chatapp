61894bcb8589242ea71ca9f6834b06aeb7aff354
Jyarn <58491036+Jyarn@users.noreply.github.com>
2025-10-15 01:32:45 -0400

    add postgres + docker compose

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,15 @@ import express from "express";
 import "dotenv/config";
 
 import { usersRouter } from "./routers/users_router.js";
+import { pool } from "./db.js";
 
 const app = express();
 
-app.use(express.json());
 
+app.use(express.json());
 app.use("/users", usersRouter);
 
 app.listen(process.env.BACKEND_PORT, (err) => {
   if (err) console.error(`Unable to connect: ${err}`);
   else console.log(`Connected to port ${process.env.BACKEND_PORT}.`);
-})
+});
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
+source .env
 docker compose up --build -d
-./scripts/test.sh
